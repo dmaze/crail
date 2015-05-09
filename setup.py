1dbe14ec8623db283e06cdc228f869f5d4020ffc
David Z. Maze <dzmaze@gmail.com>
2015-05-09 10:39:45 -0400

    Version -> 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='crail',
-    version='0.0.0',
+    version='0.0.1',
     description='Crayon rails online helper',
     url='https://github.com/dmaze/crail',
     author='David Maze',
